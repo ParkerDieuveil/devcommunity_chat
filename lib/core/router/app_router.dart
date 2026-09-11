14cfccd455a240392c3b9b9ec2eb1d2c69c57c92
BenDiya07 <benitdiyavanga@gmail.com>
2026-09-11 15:09:07 +0000

    feat(router): remove unused ChatPage import from app_router.dart

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,7 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../features/auth/presentation/pages/home_page.dart';
-import '../../features/chat/presentation/pages/chat_page.dart';
 import '../../features/auth/presentation/pages/login_page.dart';
 import '../../features/auth/presentation/pages/register_page.dart';
 import '../../features/auth/presentation/providers/auth_provider.dart';
